30a00a0538d122bac765b8d1500af795d1601395
at21xx0 <2129380280@qq.com>
2023-03-16 14:45:46 +0800

    fix cmake condition

--- cmake/s/s/src/s2/s2.c
+++ cmake/s/s/src/s2/s2.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "s2.h"
 
+#if !defined(s2_VERSION) && defined(s2_win32_VERSION)
+#define s2_VERSION s2_win32_VERSION
+#endif
 char *S2_VERSION = s2_VERSION; //默认定义
 
 int s2(int i) {
